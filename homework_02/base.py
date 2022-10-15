195560ef40d1e27e5faca6ed7f00abfda4d4d6da
l <leonid9600@gmail.com>
2022-10-15 13:57:55 +0300

    add exceptions edited base.py class Vehicle add def start

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,18 @@
 from abc import ABC
+from homework_02.exceptions import LowFuelError, NotEnoughFuel
 
 
 class Vehicle(ABC):
-    pass
+    started = False
+
+    def __init__(self, weight=10, fuel=40, fuel_consumption=1):
+        self.weight = weight
+        self.fuel_consumption = fuel_consumption
+        self.fuel = fuel
+
+    def start(self):
+        if not self.started:
+            if self.fuel > 0:
+                self.started = True
+            else:
+                raise LowFuelError
